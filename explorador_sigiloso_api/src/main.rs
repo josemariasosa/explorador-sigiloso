24b9ebc8e2263e85d96cc20d5a5d039b57d2c418
José María Sosa Resendiz <jose.skedar@gmail.com>
2025-04-18 12:46:35 -0600

    new api address

--- explorador_sigiloso_api/src/main.rs
+++ explorador_sigiloso_api/src/main.rs
@@ -17,7 +17,7 @@ use axum::{
 async fn main() {
     dotenvy::dotenv().expect("Failed to load .env file");
 
-    let bitcoin_url = env::var("BTC_RPC_URL").expect("BTC_RPC_URL not set");
+    let explorador_url = env::var("API_SERVER_BIND").expect("API_SERVER_BIND not set");
     let rpc_client = Arc::new(create_rpc_client());
     let app_state = rpc_client.clone();
 
@@ -27,7 +27,7 @@ async fn main() {
         .with_state(app_state);
 
     // run our app with hyper, listening globally on port 3000
-    let listener = tokio::net::TcpListener::bind(bitcoin_url).await.unwrap();
+    let listener = tokio::net::TcpListener::bind(explorador_url).await.unwrap();
     println!("🚀 Explorador Sigiloso API running at http://{:?}/", listener.local_addr().unwrap());
     axum::serve(listener, app).await.unwrap();
 }
